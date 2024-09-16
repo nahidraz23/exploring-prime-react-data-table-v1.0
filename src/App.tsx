d6e7c10aa5712d7e00771e2a8864c3a8a2d32db6
nahidraz23 <nzamanraz@gmail.com>
2024-09-16 22:19:45 +0600

    select only current page and overlay pannel implemented without functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import axios from "axios";
 import "./App.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
-import { InputSwitch, InputSwitchChangeEvent } from 'primereact/inputswitch';
+import { Paginator, PaginatorPageChangeEvent } from "primereact/paginator";
+import "primeicons/primeicons.css";
+import { OverlayPanel } from "primereact/overlaypanel";
+import { Button } from "primereact/button";
+import { InputText } from "primereact/inputtext";
 
 interface DataInfo {
   title: string;
@@ -13,22 +17,83 @@ interface DataInfo {
   date_start: number;
   date_end: number;
   id: number;
+  total: number;
+  currentPage: number;
 }
 
 function App() {
   const [data, setData] = useState<DataInfo[]>([]);
   // const [rowClick, setRowClick] = useState(true);
-  const [selectedRow, setSelectedRow] = useState<DataInfo[] | null >(null)
-  // console.log(data);
+  const [selectedRow, setSelectedRow] = useState<DataInfo[] | null>([]);
+  const [selected, setSelected] = useState(0);
+  const [pagination, setPagination] = useState<DataInfo[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+  const [first, setFirst] = useState(0);
+  const [rows, setRows] = useState(5);
+
+  const pageNo: number = currentPage + 1;
+  const totalRecords: number = pagination?.total;
+  const op = useRef(null);
+
+  console.log(selectedRow)
 
   const fetchData = async () => {
-    const res = await axios.get("https://api.artic.edu/api/v1/artworks?page=1");
-    return setData(res.data.data);
+    const res = await axios.get(
+      `https://api.artic.edu/api/v1/artworks?page=${currentPage + 1}`
+    );
+    setData(res.data.data);
+
+    return setLoading(false);
+  };
+
+  const paginationData = async () => {
+    const res = await axios.get("https://api.artic.edu/api/v1/artworks");
+    return setPagination(res?.data?.pagination);
   };
 
   useEffect(() => {
     fetchData();
-  }, []);
+    paginationData();
+  }, [pageNo]);
+
+  const onPageChange = (event: PaginatorPageChangeEvent) => {
+    setFirst(event.first);
+    setRows(event.rows);
+    setCurrentPage(event.page);
+  };
+
+  const handleOnsubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+    const inputValue: number = parseInt(form.inputValue.value);
+    setSelected(inputValue)
+  }
+
+  // Custom header template with a button that triggers the OverlayPanel
+  const headerTemplate = () => {
+    return (
+      <div className="table-header">
+        <Button
+          icon="pi pi-angle-down"
+          onClick={(e) => op.current.toggle(e)}
+          className="p-button-primary"
+        />
+        <OverlayPanel
+          ref={op}
+          id="overlayPanel"
+          style={{ width: "300px" }}
+          className=""
+        >
+          <form onSubmit={handleOnsubmit}>
+            <InputText type="number" name="inputValue" className="border-2 border-gray-500 p-2" placeholder="Select Rows...."></InputText>
+            <input type="submit" value="Submit" className="border-2 border-gray-600 mt-2 p-1 rounded-md cursor-pointer"/>
+          </form>
+        </OverlayPanel>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -37,24 +102,31 @@ function App() {
           Exploring PRIME<span className="text-cyan-600">REACT</span> Data-table
         </h1>
       </div>
-      <div>
+      <div className="">
         <DataTable
           value={data}
-          paginator
-          rows={10}
+          rows={5}
           rowsPerPageOptions={[5, 10, 25, 50]}
           tableStyle={{ minWidth: "50rem" }}
-          // selectionMode={rowClick ? undefined : 'multiple'}
-          selection={selectedRow!}
+          selection={selectedRow}
           onSelectionChange={(e) => setSelectedRow(e.value)}
           dataKey="id"
+          selectionPageOnly
+          loading={loading}
         >
-          <Column selectionMode="multiple" headerStyle={{ width: '3rem' }}></Column>
+          <Column
+            selectionMode="multiple"
+            headerStyle={{ width: "3rem" }}
+          ></Column>
+
+          <Column header={headerTemplate()}></Column>
+
           <Column
             field="title"
             header="Title"
             style={{ width: "25%" }}
           ></Column>
+
           <Column
             field="place_of_origin"
             header="place_of_origin"
@@ -81,6 +153,12 @@ function App() {
             style={{ width: "25%" }}
           ></Column>
         </DataTable>
+        <Paginator
+          first={first}
+          rows={rows}
+          totalRecords={totalRecords}
+          onPageChange={onPageChange}
+        />
       </div>
     </>
   );
